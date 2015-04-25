8d21cd42d54b7f1a66ed1083c1248e1332b73f72
Takeshi Ueda <uedatakeshi@gmail.com>
2015-04-25 09:48:08 +0900

    ログイン直後の画面をreportsに変更

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :reports
   resources :users
 	
-	root to: "home#index"
+	root to: "reports#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

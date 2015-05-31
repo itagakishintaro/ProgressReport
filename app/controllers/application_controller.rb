18cbfca327b71e577c7a981ab4918f6b24bd1eeb
itagaki <itagaki.shintaro@gmail.com>
2015-05-31 12:20:41 +0900

    apiのテストを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :unless => :api?
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
-	  devise_parameter_sanitizer.for(:account_update) << :name
+      devise_parameter_sanitizer.for(:account_update) << :name
+    end
+
+  private
+    def api?
+      request.path_info.match(/^\/api\//)
     end
 end
---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'UsersController', type: :request do
+  it 'sends users with progresses' do
+    FactoryGirl.create(:user, {id: 1})
+    FactoryGirl.create(:user, {id: 2})
+    FactoryGirl.create(:user, {id: 3})
+    FactoryGirl.create(:progress, {user_id: 1})
+    FactoryGirl.create(:progress, {user_id: 2})
+    FactoryGirl.create(:progress, {user_id: 2})
+    FactoryGirl.create(:progress, {user_id: 2})
+    get '/api/users/with_progresses'
+    expect(response).to have_http_status(:success)
+    json = JSON.parse(response.body)
+    expect(json.size).to eq 2
+    expect(json[0]['id']).to eq 2
+    expect(json[0]['point']).to eq 3
+    expect(json[1]['id']).to eq 1
+    expect(json[1]['point']).to eq 1
+  end
+end

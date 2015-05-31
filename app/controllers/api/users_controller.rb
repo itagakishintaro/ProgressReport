43f3d0569ed671728f476e808e2e9688a0cce66c
itagaki <itagaki.shintaro@gmail.com>
2015-05-31 13:07:37 +0900

    # This is a combination of 2 commits. # The first commit's message is:
    herokuで動かないので試しにactive recordの書き方を修正
    
    # This is the 2nd commit message:
    
    heroku用に試し修正

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class UsersController < ApplicationController
     def with_progresses
-      @user_with_progresses = User.select('users.id, users.name, SUM(progresses.point) AS point, progresses.updated_at').joins(:progresses).group('users.id').order('point desc')
+      @user_with_progresses = User.with_progress_points
       render :json => @user_with_progresses.to_json
     end
   end

0df1489e000c7742e04de094bcd768f031366505
itagaki <itagakishintaro@jp.fujitsu.com>
2015-07-31 11:00:32 +0900

    コメントバック用のModelを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,13 @@ class Comment < ActiveRecord::Base
     .where("reports.user_id = #{user_id}")
     .order('comments.updated_at desc')
   end
+
+	# 自分がコメントしたレポートのうち、誰かがコメントしたレポートのIDとコメントの更新日付の最大値を取得
+	def self.back_for_user(user_id)
+		select('comments.report_id AS report_id, max(comments.updated_at) AS updated_at')
+		# 自分がコメントしたレポート　かつ　自分のコメントよりも後のコメント　に限定
+		.joins("INNER JOIN (SELECT report_id, updated_at FROM COMMENTS WHERE user_id = #{user_id}) AS SELF ON SELF.report_id = comments.report_id AND SELF.updated_at < comments.updated_at")
+		# 自分のコメントを除外
+		.where("comments.user_id != #{user_id}")
+		.group('comments.report_id, comments.updated_at')
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -21,5 +21,14 @@ describe Comment do
         expect(Comment.for_user(1)[1].user_id).to eq 4
       end
     end
+
+		describe 'self.back_for_user' do
+      it 'returns others comments to commented reports for a user' do
+        expect(Comment.back_for_user(4).size).to eq 1
+        expect(Comment.back_for_user(4)[0].report_id).to eq 1
+        expect(Comment.back_for_user(4)[0].updated_at).to eq 0.days.ago
+				expect(Comment.back_for_user(1).size).to eq 0
+      end
+    end
 	end
 end

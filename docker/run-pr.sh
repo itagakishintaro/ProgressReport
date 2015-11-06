3056f43d7c5af7fde4c10dac222c9834581b033d
itagaki <itagaki.shintaro@gmail.com>
2015-11-06 16:05:09 +0900

    Add Device secret for docker

--- docker/run-pr.sh
+++ docker/run-pr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd /home/pr/ProgressReport
-rails console -e docker
-bundle install
+# http://ayumu-homes.hateblo.jp/entry/2015/02/08/174838
+gem install nokogiri -- --use-system-libraries=true --with-xml2-include=/usr/include/libxml2/
+bundle install --without test development
 bundle update
 bundle exec rake db:migrate RAILS_ENV=docker
